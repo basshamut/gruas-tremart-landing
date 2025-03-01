2ad1991db6ce049ec4e707a2e1e63d21391b3f5c
Jesus Fajardo <es.jfajardobass@gmail.com>
2025-03-01 16:22:52 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        lucide: ['*'],
+        mdi: ['facebook', 'twitter', 'instagram']
+      }
+    })
+  ],
+  image: {
+    domains: ['placeholder.com'],
+    remotePatterns: [{ protocol: "https" }]
+  }
+});
+
